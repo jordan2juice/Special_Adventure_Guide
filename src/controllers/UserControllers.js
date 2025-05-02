c35810dc1ff85e805c705b59d5d0ea0824300b27
jordan toujouse <jtoujo1@gmail.com>
2025-05-02 15:01:57 -0500

    Implements home endpoint for user authentication
    Adds a new endpoint for user authentication, which generates a secure token using JWT and returns it to the client. This allows users to authenticate and access protected routes.
    
    Fixes #789

--- src/controllers/UserControllers.js
+++ src/controllers/UserControllers.js
@@ -3,4 +3,18 @@
 const signToken  = require("../middleware/loginMiddle");
 const User = require("../modals/User");
 
+const home = async (req, res) => {
+    try {
+        const token = await signToken(req, res, next);
+        res.send(token);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    home,
+};  
+
+
 
